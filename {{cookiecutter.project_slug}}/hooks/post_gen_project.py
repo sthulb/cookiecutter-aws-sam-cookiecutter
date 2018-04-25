e9590887ee630eef401ecdb79a8e89a034974e66
Simon Thulbourn <simon+github@thulbourn.com>
2018-04-25 10:25:51 +0100

    fix project hook

--- {{cookiecutter.project_slug}}/hooks/post_gen_project.py
+++ {{cookiecutter.project_slug}}/hooks/post_gen_project.py
@@ -1,6 +1,6 @@
 """
-    Given that this cookiecutter will run to scaffold a Python
-    Lambda project and the user may not want to use the Makefile
+    Given that this cookiecutter will run to scaffold a cookiecutter
+    template project and the user may not want to use the Makefile
     so we will remove it if that's the case.
 """
 
@@ -13,24 +13,9 @@ INFO = "\x1b[1;33m [INFO]: "
 SUCCESS = "\x1b[1;32m [SUCCESS]: "
 HINT = "\x1b[3;33m"
 
-
-def remove_optional_files():
-    filenames = ["Makefile"]
-    for file in filenames:
-        if os.path.isfile(file):
-            print(INFO + "Removing {} from project due to chosen options...".
-                  format(file) + TERMINATOR)
-            os.remove(file)
-
-    return True
-
-
 def main():
 
-    project_slug = '{{ cookiecutter.project_slug }}'
-    makefile_choice = '{{ cookiecutter.include_experimental_make }}'.lower()
-    if makefile_choice == 'n':
-        remove_optional_files()
+    project_slug = '{{ cookiecutter.project_name }}'
 
     print(SUCCESS +
           "Project initialized successfully! You can now jump to {} folder".
